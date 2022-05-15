2a1f7319fd6a4fc35b19ab7c0c8a9e43999c06b7
Kevin Garcia Ramirez <mr.kegara@gmail.com>
2022-05-15 12:44:39 -0500

    info section

---
+++ src/views/FishInfo.vue
@@ -0,0 +1,77 @@
+<template>
+  <section>
+    <div class="TitleContainer" style="text-align: center">
+      <h1>Ballena Azul</h1>
+    </div>
+
+    <div style="overflow: auto">
+      <div class="TextContainer">
+        <p>
+          La ballena azul es el mamífero más grande del mundo, pueden llegar a
+          medir más de 30 metros y pesar 200 toneladas, ¡lo equivalente a 33
+          elefantes! <br />
+          <br />
+          Habitan en todos los océanos del mundo y viven entre 80 y 90 años.
+          <br />
+          <br />
+          Se ven afectadas sobretodo por la cantidad enorme de plástico que hay
+          en los océanos, porque muchas veces llegan a consumir bolsas de
+          plástico y botellas, lo que les puede causar heridas muuy graves o
+          incluso la muerte. <br />
+          <br />
+          Además, el cambio climático afecta mucho al kril, que es su principal
+          alimento. Y, al igual que los leones marinos, son atrapadas y
+          lastimadas sin querer por barcos pesqueros.
+        </p>
+      </div>
+
+      <div class="GifContainer">
+        <img src="./src/assets/images/Blue_Whale/Blue_Whale.gif" />
+      </div>
+    </div>
+
+    <div class="titleBox">
+      <h1>¿Dónde viven las ballenas azules?</h1>
+    </div>
+
+    <div class="MapContainer">
+      <img
+        src="./src/assets/images/Blue_Whale/blue-whale-range-map.webp"
+        style="width: 80vw; min-width: 330px"
+        alt="WebP rules."
+      />
+    </div>
+
+    <div class="TextReferences">
+      <h2 style="color: rgb(5, 48, 141)">Referencias:</h2>
+      <br />
+      <p>
+        1. Blue Whale. Facts. (n.d.). Revisado el 15 Mayo de 2022, de
+        <a
+          href="https://www.worldwildlife.org/species/blue-whale"
+          target="_blank"
+          style="color: black"
+          >https://www.worldwildlife.org/species/blue-whale</a
+        >
+        <br />
+        <br />
+
+        2. Blue Whale. Animals. (n.d.). Revisado el 15 Mayo de 2022, de
+        <a
+          href="https://kids.nationalgeographic.com/animals/mammals/facts/blue-whale"
+          target="_blank"
+          style="color: black"
+          >https://kids.nationalgeographic.com/animals/mammals/facts/blue-whale
+        </a>
+        <br />
+        <br />
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
